ac9896d40a416ec2a0dc8b46172249e540a51673
Jason Brewer <jlewisbrewer@gmail.com>
2021-02-23 11:59:13 -0800

    Added a simple post request

--- library_api/server.py
+++ library_api/server.py
@@ -1,10 +1,9 @@
 # Connect to the database
-from flask import Flask, request
+from flask import Flask, request, Response
 import json
 from utils.load_database import load_database
 from utils.connect_database import connect_database
-from utils.search_database import get_all_books, get_book_by_params
-from models.book import Book
+from utils.search_database import get_all_books, get_book_by_params, get_book_by_id
 
 app = Flask(__name__)
 iris_native = connect_database()
@@ -17,13 +16,33 @@ def get_books():
     books = iris_native.iterator(book_db)
     books_to_return = []
     
-    print(request.args)
     if request.args:
         books_to_return = get_book_by_params(iris_native, books, book_db, request.args)
     else:
         books_to_return = get_all_books(iris_native, books, book_db)
 
     return json.dumps([book.__dict__ for book in books_to_return])
+
+# Update book by id
+@app.route('/update/book/<id>', methods=['POST'])
+def update_book(id):
+    book_db = "^books"
+    field = "available"
+
+    # This outer conditional does not work as intended, will set values
+    # on books that don't exist
+    if get_book_by_id(iris_native, book_db, id):
+        if iris_native.get(book_db, id, field):
+            iris_native.set(False, book_db, id, field)
+        else:
+            iris_native.set(True, book_db, id, field)
+        return Response('Changed book availablitiy', status=200)
+
+    return Response('Unable to find book', status=404)
+
+
+
+
 
 
 if __name__ == '__main__':
